b671a50253c4b55ea7c9dcce6855eca95d0affac
Navid Farahzadi <na.farahzadi@gmail.com>
2017-07-01 19:03:30 +0430

    some functions added to chess helper

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      model: [0, 0, 0, 0, 0, 0, 0, 0],
+      // model: [0, 0, 0, 0, 0, 0, 0, 0],
+      // model: [1, 3, 4, 0, 7, 4, 6, 2],
+      model: [1, 3, 4, 0, 6, 4, 6, 2],
+      // model: [3, 5, 7, 1, 6, 0, 2, 4],
       threats: 2
     }
   }
--- src/lib/chess-helper.js
+++ src/lib/chess-helper.js
@@ -1,9 +1,26 @@
+import _ from 'lodash'
+
 export function arrayToChessBoard(arrModel) {
-  const newModel = new Array(8).fill(new Array(8).fill(0))
-    .map((row, rowIndex) => (
-      row.map((col, colIndex) => (
-        arrModel[colIndex] === rowIndex ? 1 : 0
-      ))
-    ))
+  const newModel = new Array(8)
+    .fill(new Array(8).fill(0))
+    .map((row, rowIndex) => (row.map((col, colIndex) => (arrModel[colIndex] === rowIndex
+      ? 1
+      : 0))))
   return newModel
 }
+
+export function isMatrix(array) {
+  const size = array[0].length
+  return array.forEach(row => row.length === size)
+}
+
+export function getDiagonalIndexes(matrix) {
+  const shape = [matrix.length, matrix[0].shape]
+}
+
+export function calcFitness(state) {
+  const rowClashs = (state.length - _.uniq(state).length) * 2
+  console.log(rowClashs)
+}
+
+calcFitness([0, 0, 0, 0, 0, 0, 0, 0])
