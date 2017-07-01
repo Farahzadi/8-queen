647a6b6617eef494e237d55550f78c0cd143aef8
Navid Farahzadi <na.farahzadi@gmail.com>
2017-07-01 19:01:46 +0430

    calcfitness test added

--- src/lib/chess-helper.test.js
+++ src/lib/chess-helper.test.js
@@ -1,4 +1,4 @@
-import { arrayToChessBoard } from './chess-helper'
+import { arrayToChessBoard, calcFitness } from './chess-helper'
 
 it('check if arrayToChessBoard works fine', () => {
   expect(arrayToChessBoard([0, 0, 0, 0, 0, 0, 0, 0])).toEqual(
@@ -25,4 +25,11 @@ it('check if arrayToChessBoard works fine', () => {
       [0, 0, 0, 0, 0, 0, 0, 0]
     ]
   )
+})
+
+it('test calculating fitness from chromosome', () => {
+  expect(calcFitness([0, 0, 0, 0, 0, 0, 0, 0])).toBe(-56)
+  expect(calcFitness([1, 3, 4, 0, 7, 4, 6, 2])).toBe(-6)
+  expect(calcFitness([1, 3, 4, 0, 6, 4, 6, 2])).toBe(-12)
+  expect(calcFitness([3, 5, 7, 1, 6, 0, 2, 4])).toBe(0)
 })
